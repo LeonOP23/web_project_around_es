cffc9d18fafea9c00791c7f701d24bb996572e7f
LeonOspina <leonospina23@gmail.com>
2026-03-03 19:29:32 -0500

    Actualizar parte 2 proyecto 8

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,54 @@ const initialCards = [
 initialCards.forEach(function(card) {
   console.log(card.name);
 });
+
+
+//comienzo parte 2 proyecto
+const editButton = document.querySelector(".profile__edit-button");
+const editPopup = document.querySelector("#edit-popup");
+const editCloseButton = editPopup.querySelector(".popup__close");
+
+function openModal(modal) {
+   modal.classList.add("popup_is-opened")
+};
+
+function closeModal(modal) {
+   modal.classList.remove("popup_is-opened")
+};
+
+editButton.addEventListener("click", handleOpenEditModal);
+
+editCloseButton.addEventListener("click", () => {
+  closeModal(editPopup);
+});
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const popupTypeName = document.querySelector(".popup__input_type_name");
+const popupTypeDescription = document.querySelector(".popup__input_type_description");
+let formElement = document.querySelector("#edit-profile-form");
+
+function fillProfileForm() {
+  popupTypeName.value = profileTitle.textContent;
+  popupTypeDescription.value = profileDescription.textContent;
+};
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(editPopup);
+};
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  const nameValue = popupTypeName.value;
+  const descriptionValue = popupTypeDescription.value;
+
+  profileTitle.textContent = nameValue;
+  profileDescription.textContent = descriptionValue;
+
+
+  closeModal(editPopup);
+};
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
